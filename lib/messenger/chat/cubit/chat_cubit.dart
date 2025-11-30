3c2954a3473996c4035b752230ca1dc88e099196
Le Duong Huy <leduonghuy.7.8.5@gmail.com>
2025-11-30 15:36:04 +0700

    figbug: sửa xong lỗi tìm kiếm trong trang chatPage

--- lib/messenger/chat/cubit/chat_cubit.dart
+++ lib/messenger/chat/cubit/chat_cubit.dart
@@ -22,14 +22,14 @@ class ChatCubit extends Cubit<ChatState>{
     );
     // Lắng nghe messages từ server
     _channel!.stream.listen((message) {
-      print("Da stream voi server chat");
+      // print("Da stream voi server chat");
 
       // Parse message từ JSON
       final data = jsonDecode(message); // jsonDecode(message) có thể trả String, List, num, hoặc Map tùy payload.
       if (data is! Map<String, dynamic>) return;  // nghĩa là: nếu parsed JSON không phải object (Map) thì bỏ qua message.
       
       if(data['event'] == 'chat_updated') {
-        print("đã nhận được event chat_updated từ server");
+        // print("đã nhận được event chat_updated từ server");
         final updatedChat = ChatModel.fromJson(data['chat']);
         if (state is ChatLoadedState) {
           final current = (state as ChatLoadedState);
@@ -38,7 +38,7 @@ class ChatCubit extends Cubit<ChatState>{
         }
       }else if (data['event'] == 'presence_update') {
         String status = data['status'];
-        print("DEBUG: receiverStatus: " + status);
+        // print("DEBUG: receiverStatus: " + status);
         final current = (state as ChatLoadedState).chats;
         emit(ChatLoadedState(receiverStatus: status, chatFilter: current, chats: current));
       }
@@ -62,7 +62,7 @@ class ChatCubit extends Cubit<ChatState>{
         // Parse danh sách chat từ JSON
         List<ChatModel> chats = chatsJson
             .map((json) {
-              print('Raw chat JSON: $json');
+              // print('Raw chat JSON: $json');
               return ChatModel.fromJson(json as Map<String, dynamic>);
             })
             .toList();
@@ -84,7 +84,11 @@ class ChatCubit extends Cubit<ChatState>{
       final filtered = q.isEmpty
           ? current.chats
           : current.chats.where((m) {
-            return m.members.any((member) => member.username?.toLowerCase().contains(q) ?? false);
+            return m.members.any((member) {
+              final username = member.username ?? '';
+              final isNotSelf = member.id != currentUserId;
+              return username.toLowerCase().contains(q) && isNotSelf ;
+            });
           }).toList();
 
       emit(ChatLoadedState(chats: current.chats, chatFilter: filtered));
